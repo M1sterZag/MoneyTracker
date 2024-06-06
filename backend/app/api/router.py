40ca4a34ba8db3ae3af8b58f43876f56df9e30ce
M1sterZag <maximzaglubotsky@gmail.com>
2024-06-06 18:42:35 +0300

    refactor api routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from .users import router as users_router
 from app.auth.router import router as auth_router
+from app.operations_service.router import router as operations_router
 
 router = APIRouter(
     prefix="/api",
     tags=["All routes"],
 )
 
-# router.include_router(users_router)
 router.include_router(auth_router)
+router.include_router(operations_router)
