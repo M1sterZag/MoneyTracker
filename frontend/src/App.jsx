4533f8c405f3fff1b0777e289807ff476515d0a5
M1sterZag <maximzaglubotsky@gmail.com>
2024-06-13 19:33:40 +0300

    add token to header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import Home from './Home';
 import Operations from './Operations';
 import DownloadReport from './DownloadReport';
-import Sidebar from './Sidebar';
+import Header from './Header.jsx';
 import Login from './Login';
 import Register from './Register';
 
@@ -30,16 +30,18 @@ function AppContent({ token, setToken }) {
   const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
 
   return (
-    <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full">
-      <div className="panel bg-mcblack p-10 flex rounded-lg">
-        {!isAuthPage && <Sidebar setToken={setToken} />}
-        <Routes>
-          <Route path="/" element={<Home token={token} />} />
-          <Route path="/operations" element={<Operations token={token} />} />
-          <Route path="/download-report" element={<DownloadReport />} />
-          <Route path="/login" element={<Login setToken={setToken} />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+    <div className="flex flex-col h-full">
+      {!isAuthPage && <Header setToken={setToken} token={token} />}
+      <div className="flex items-center justify-center flex-grow">
+        <div className="panel bg-mcblack p-10 flex rounded-lg">
+          <Routes>
+            <Route path="/" element={<Home token={token} />} />
+            <Route path="/operations" element={<Operations token={token} />} />
+            <Route path="/download-report" element={<DownloadReport />} />
+            <Route path="/login" element={<Login setToken={setToken} />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
