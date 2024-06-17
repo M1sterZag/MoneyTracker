8375316d758682b3c8bb7f8eab1de9f23cec31f0
M1sterZag <maximzaglubotsky@gmail.com>
2024-06-17 17:58:26 +0300

    add download reports like png

--- frontend/src/Graphics.jsx
+++ frontend/src/Graphics.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import html2canvas from 'html2canvas';
 import {
   LineChart,
   Line,
@@ -112,13 +113,28 @@ function Graphics({ token }) {
     }
   }, [token]);
 
+  const handleDownloadReports = () => {
+    const chartsContainer = document.getElementById('charts-container');
+
+    if (chartsContainer) {
+      html2canvas(chartsContainer).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = imgData;
+        link.download = 'reports.png';
+        link.click();
+      });
+    }
+  };
+
   return (
     <div className="w-full p-6 bg-mcgray rounded-lg">
-      <div className="mb-5">
-        <div className="bg-mcblack p-4 rounded-lg shadow-lg">
-          <h3 className="text-2xl text-center text-white mb-4">Доходы и расходы по месяцам</h3>
-          <ResponsiveContainer width="100%" height={400}>
-            <LineChart
+      <div id="charts-container" className="bg-mcgray">
+        <div className="mb-5">
+          <div className="bg-mcblack p-4 rounded-lg shadow-lg">
+            <h3 className="text-2xl text-center text-white mb-4">Доходы и расходы по месяцам</h3>
+            <ResponsiveContainer width="100%" height={400}>
+              <LineChart
                 data={monthlyData}
                 margin={{
                   top: 5,
@@ -126,29 +142,29 @@ function Graphics({ token }) {
                   left: 20,
                   bottom: 5,
                 }}
-            >
-              <CartesianGrid strokeDasharray="3 3"/>
-              <XAxis dataKey="month"/>
-              <YAxis/>
-              <Tooltip/>
-              <Legend
-                payload={[
-                  { value: 'Доходы', type: 'line', id: 'income', color: '#6CB54A' },
-                  { value: 'Расходы', type: 'line', id: 'expense', color: '#FF0303' },
-                ]}
-              />
-              <Line type="monotone" dataKey="income" stroke="#6CB54A"/>
-              <Line type="monotone" dataKey="expense" stroke="#FF0303"/>
-            </LineChart>
-          </ResponsiveContainer>
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend
+                  payload={[
+                    { value: 'Доходы', type: 'line', id: 'income', color: '#6CB54A' },
+                    { value: 'Расходы', type: 'line', id: 'expense', color: '#FF0303' },
+                  ]}
+                />
+                <Line type="monotone" dataKey="income" stroke="#6CB54A" />
+                <Line type="monotone" dataKey="expense" stroke="#FF0303" />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
         </div>
-      </div>
 
-      <div className="mb-5">
-        <div className="bg-mcblack p-4 rounded-lg shadow-lg">
-          <h3 className="text-2xl text-center text-white mb-4">Общие доходы и расходы</h3>
-          <ResponsiveContainer width="100%" height={400}>
-            <BarChart
+        <div className="mb-5">
+          <div className="bg-mcblack p-4 rounded-lg shadow-lg">
+            <h3 className="text-2xl text-center text-white mb-4">Общие доходы и расходы</h3>
+            <ResponsiveContainer width="100%" height={400}>
+              <BarChart
                 data={totalData}
                 margin={{
                   top: 5,
@@ -156,60 +172,88 @@ function Graphics({ token }) {
                   left: 20,
                   bottom: 5,
                 }}
-            >
-              <CartesianGrid strokeDasharray="3 3"/>
-              <XAxis dataKey="name"/>
-              <YAxis/>
-              <Tooltip/>
-              <Legend verticalAlign="bottom" align="center"
-                payload={[
-                  { value: 'Доходы', type: 'bar', id: 'income', color: '#6CB54A' },
-                  { value: 'Расходы', type: 'bar', id: 'expense', color: '#FF0303' },
-                ]}
-              />
-              <Bar dataKey="income" fill="#6CB54A"/>
-              <Bar dataKey="expense" fill="#FF0303"/>
-            </BarChart>
-          </ResponsiveContainer>
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend
+                  verticalAlign="bottom"
+                  align="center"
+                  payload={[
+                    { value: 'Доходы', type: 'bar', id: 'income', color: '#6CB54A' },
+                    { value: 'Расходы', type: 'bar', id: 'expense', color: '#FF0303' },
+                  ]}
+                />
+                <Bar dataKey="income" fill="#6CB54A" />
+                <Bar dataKey="expense" fill="#FF0303" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full md:w-1/2 px-4">
+            <div className="bg-mcblack rounded-lg shadow-lg mb-5">
+              <h3 className="text-2xl text-center text-white">Топ категорий доходов</h3>
+              <ResponsiveContainer width="100%" height={300}>
+                <PieChart>
+                  <Pie
+                    data={incomeData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    fill="#6CB54A"
+                    label
+                  >
+                    {incomeData.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                  </Pie>
+                  <Legend layout="vertical" verticalAlign="middle" align="right" />
+                  <Tooltip />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+
+          <div className="w-full md:w-1/2 px-4">
+            <div className="bg-mcblack rounded-lg shadow-lg mb-5">
+              <h3 className="text-2xl text-center text-white">Топ категорий расходов</h3>
+              <ResponsiveContainer width="100%" height={300}>
+                <PieChart>
+                  <Pie
+                    data={expenseData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    fill="#FF0303"
+                    label
+                  >
+                    {expenseData.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                  </Pie>
+                  <Legend layout="vertical" verticalAlign="middle" align="right" />
+                  <Tooltip />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-wrap -mx-4">
-        <div className="w-full md:w-1/2 px-4">
-          <div className="bg-mcblack rounded-lg shadow-lg">
-            <h3 className="text-2xl text-center text-white">Топ категорий доходов</h3>
-            <ResponsiveContainer width="100%" height={300}>
-              <PieChart>
-                <Pie data={incomeData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} fill="#6CB54A"
-                     label>
-                  {incomeData.map((entry, index) => (
-                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-                  ))}
-                </Pie>
-                <Legend layout="vertical" verticalAlign="middle" align="right"/>
-                <Tooltip/>
-              </PieChart>
-            </ResponsiveContainer>
-          </div>
-        </div>
-
-        <div className="w-full md:w-1/2 px-4">
-          <div className="bg-mcblack rounded-lg shadow-lg">
-            <h3 className="text-2xl text-center text-white">Топ категорий расходов</h3>
-            <ResponsiveContainer width="100%" height={300}>
-              <PieChart>
-                <Pie data={expenseData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100}
-                     fill="#FF0303" label>
-                  {expenseData.map((entry, index) => (
-                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-                  ))}
-                </Pie>
-                <Legend layout="vertical" verticalAlign="middle" align="right"/>
-                <Tooltip/>
-              </PieChart>
-            </ResponsiveContainer>
-          </div>
-        </div>
+      <div className="text-center">
+        <button
+          className="bg-mcblack text-white p-3 rounded-lg border-none hover:bg-mgreen cursor-pointer ml-3 text-base font-medium transition-colors duration-300 whitespace-nowrap"
+          onClick={handleDownloadReports}
+        >
+          Скачать отчеты
+        </button>
       </div>
     </div>
   );
